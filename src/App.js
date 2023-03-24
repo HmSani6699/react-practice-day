1bd5f69fbd9e920a3a2a419392542761773e9d12
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-24 11:49:59 +0600

    counter part complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -14,13 +15,29 @@ const displayStyle = {
   border: '2px solid',
   backgroundColor: 'orange',
   margin: '20px',
-  borderRadius: '20px'
+  borderRadius: '20px',
+  padding: '20px',
 }
+const binStyle = {
+  padding: '8px 15px',
+  borderRadius: '15px',
+  border: 'none',
+  cursor: 'pointer'
+}
+
 function DisplayInfo(props) {
+  const [counter, setCounter] = useState(0);
+
+  const powerCounter = () => {
+    const newCounter = counter + 1;
+    setCounter(newCounter)
+  }
   return (
     <div /* className='display-info' */ style={displayStyle}>
-      <h1 style={{ color: 'cadetblue' }}>Name: {props.name}</h1>
+      <h1 style={{ color: 'blue' }}>Name: {props.name}</h1>
       <p>Address: {props.address}</p>
+      <h4>Power: {counter}</h4>
+      <button style={binStyle} onClick={powerCounter}>Boost The Power</button>
     </div>
   )
 }
